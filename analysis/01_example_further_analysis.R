2eff7979171aebd5a1e3a3486cc280c965402082
OJWatson <o.watson15@imperial.ac.uk>
2025-07-04 14:14:41 +0100

    small change to 01

--- analysis/01_example_further_analysis.R
+++ analysis/01_example_further_analysis.R
@@ -5,3 +5,4 @@ df <- readRDS("analysis/data/derived_data/example.rds")
 
 ## Because this R file needs the example.rds file to exist to run, it must
 # be run after 00_example_analysis_file.R, so it is numbered after 00 (i.e. 01_)
+df$d <- df$a + 10
